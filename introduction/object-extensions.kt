b3e4eaff51639962e93bc3f877147854d734314d
Favio Manriquez <favio@favrik.com>
2020-06-14 16:32:36 -0700

    Add introduction -> object extensions

---
+++ introduction/object-extensions.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun getList(): List<Int> {
+    val arrayList = arrayListOf(1, 5, 2)
+    Collections.sort(arrayList, object: Comparator<Int> { 
+        override fun compare(a: Int, b: Int): Int {
+            return b - a
+        }
+    })
+    return arrayList
+}
