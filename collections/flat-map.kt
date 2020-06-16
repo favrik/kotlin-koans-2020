766970decbff11750854d23c53c14d0ef5fb0b75
Favio Manriquez <favio@favrik.com>
2020-06-15 22:02:31 -0700

    Add collections -> flat map

---
+++ collections/flat-map.kt
@@ -0,0 +1,9 @@
+// Return all products this customer has ordered
+val Customer.orderedProducts: Set<Product> get() {
+    return orders.flatMap {  it.products }.toSet()
+}
+
+// Return all products that were ordered by at least one customer
+val Shop.allOrderedProducts: Set<Product> get() {
+    return customers.flatMap { it.orders.flatMap { it.products } }.toSet()
+}
